1a494c2b5c00f6c287a75411e8a68e32c7b50558
Ashish Khatri <khatri8.ashish@gmail.com>
2022-07-04 01:10:14 +0530

    Added User creation logic for db

---
+++ services/createNFT.js
@@ -0,0 +1,22 @@
+import NFT from "../models/nftModel.js";
+
+const createNft = async (name, description, price, imageStaticUrl, imageIpfsUrl, minter, voucher, metadataUri) => {
+    
+    // Creates a new NFT entry in the database.
+    const nft = new NFT({
+        name: name,
+        description: description,
+        imageStaticUrl: imageStaticUrl,
+        imageIpfsUrl: imageIpfsUrl,
+        ethPrice: price,
+        listedForSale: false,
+        isSold: false,
+        minter: minter,
+        signature: voucher,
+        metadataUri: metadataUri
+    });
+    return await nft.save();
+    
+};
+
+export default createNft;
